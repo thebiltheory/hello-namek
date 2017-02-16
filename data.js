f236157b7bf6897b7012dbac4b0c52f8fdc72e0d
Bil Benhamou <bil@propertyfinder.ae>
2017-02-16 19:42:02 +0400

    feat: import some data

---
+++ data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    "id": 1,
+    "first_name": "Bil",
+    "last_name": "Benhamou",
+    "email": "me@thebiltheory.com",
+    "website": "http://thebiltheory.com"
+  },
+  {
+    "id": 2,
+    "first_name": "Krillin",
+    "last_name": "unknown",
+    "email": "krillin@hello-namek.io",
+    "website": "http://hello-namek.io"
+  },
+  {
+    "id": 3,
+    "first_name": "Luffy",
+    "last_name": "Monkey D.",
+    "email": "luffy@hello-namek.io",
+    "website": "http://hello-namek.io"
+  },
+  {
+    "id": 4,
+    "first_name": "Goku",
+    "last_name": "unknown",
+    "email": "goku@hello-namek.io",
+    "website": "http://hello-namek.io"
+  },
+  {
+    "id": 5,
+    "first_name": "Natsu",
+    "last_name": "Dragneel",
+    "email": "natsu@hello-namek.io",
+    "website": "http://hello-namek.io"
+  }
+];
+
+module.exports = data;
