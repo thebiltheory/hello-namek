64356eed841fe25315fab5c128de12df30c523a1
Bil Benhamou <bil.benhamou@gmail.com>
2017-02-16 21:51:20 +0400

    feat: POST/PUT & DELETE Endpoints

--- server.js
+++ server.js
@@ -36,6 +36,60 @@ app.get('/api/contacts', (request, response) => {
      response.json(contact[0]);
  });
 
+// POST
+ app.post('/api/contacts', (request, response) => {
+
+    const contact = {
+        id: contacts.length + 1,
+        first_name: request.body.first_name,
+        last_name: request.body.last_name,
+        email: request.body.email,
+        website: request.body.website
+    }
+
+    contacts.push(contact);
+
+    response.json(contact);
+ });
+
+ // PUT
+app.put('/api/contact/:id', (request, response) => {
+
+    const requestId = request.params.id;
+
+    let contact = contacts.filter(contact => {
+        return contact.id == requestId;
+    })[0];
+
+    const index = contacts.indexOf(contact);
+
+    const keys = Object.keys(request.body);
+
+    keys.forEach( key => {
+        contact[key] = request.body[key];
+    });
+
+    contacts[index] = contact;
+
+    response.json(contacts[index]);
+});
+
+// DELETE
+app.delete('/api/contacts/:id', (request, response) => {
+    const requestId = request.params.id;
+
+    let contact = contacts.filter( contact => {
+        return contact.id == requestId;
+    })[0];
+
+    const index = contacts.indexOf(contact);
+
+    contacts.splice(index, 1);
+
+    response.json({ message: `User ${requestId} has been deleted.`})
+});
+
+
 const hostname = 'localhost';
 const port = 3001;
 
